1e4ebbf7f5bd022d957a3790fda9523ea8994262
Marc Rizzen <marcrizzend@gmail.com>
2026-01-03 02:47:17 +0800

    initial commit

---
+++ app/src/main/java/com/example/splititapp/fragments/AddPage.java
@@ -0,0 +1,32 @@
+package com.example.splititapp.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.example.splititapp.R;
+
+public class AddPage extends Fragment {
+
+    View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        view = inflater.inflate(R.layout.fragment_add_page, container, false);
+
+        return view;
+    }
+}
